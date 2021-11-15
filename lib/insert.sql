2eb84d06a55d8b7b3fe11aae9c04b220a52dd49a
D-Justice <justice.damian96@gmail.com>
2021-11-16 06:12:07 +1100

    Assignment Complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+ ('Mr. Chocolate', 32, 'M', 'Black', 'Angry', 'true'),
+  ('Rowdy', 32, 'F', 'Black', 'Angry', 'true'),
+  ('Tabitha', 32, 'F', 'Black', 'Angry', 'true'),
+  ('Sergeant Brown', 32, 'M', 'Black', 'Angry', 'true'),
+  ('Melissa', 32, 'F', 'Black', 'Angry', 'true'),
+  ('Grinch', 32, 'M', 'Black', 'Angry', 'true'),
+  ('Bill', 32, 'M', 'Black', 'Angry', 'true'),
+  (NULL, 32, 'F', 'Black', 'Angry', 'true');
